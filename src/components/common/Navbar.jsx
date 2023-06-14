55863caa150243fe23c9bb99c2f388513578a8dd
vinit <khollam.vinit@gmail.com>
2023-06-15 01:05:51 +0530

    Fixed Navbar Responsiveness

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -3,17 +3,17 @@ import logo from '../../assets/logo.svg'
 import Button from "./button"
 function Navbar() {
   return (
-    <div className="w-full py-5 ">
+    <div className="w-full border border-b border-b-gray-200 py-5 ">
         <Container>
-            <div className="w-full flex justify-between items-center">
+            <div className="w-full flex justify-center  md:justify-between items-center">
                 <img src={logo} alt="" />
                
-                <div className="links flex justify-center items-center">
+                <div className="links md:flex hidden  justify-center items-center">
                   <a className="px-2" href="">Home</a>
                   <a className="px-2" href="">About Us</a>
                   <a className="px-2" href="">Company</a>
-
                 </div>
+
             </div>
         </Container>
     </div>
